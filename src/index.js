365e5b264b1e1b5406993b2dab37fd0b20d3c984
John Lauer <jlauer12@gmail.com>
2021-06-07 09:25:53 -0700

    try1

--- src/index.js
+++ src/index.js
@@ -7,5 +7,6 @@ document.getElementById("app").innerHTML = `
   info about Parcel 
   <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
 </div>
-this is inside the jscript
+this is inside the jscript xx
+
 `;
